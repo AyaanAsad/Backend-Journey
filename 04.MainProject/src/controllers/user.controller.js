e26301e9e041578e569355eb0e78244312f5d38c
unknown <ayaan.asad.2104@gmail.com>
2026-06-17 23:41:16 +0530

    Mongo aggregate pipelines to fetch subscribers, subscribedTo and subscriberd Yes or No

--- 04.MainProject/src/controllers/user.controller.js
+++ 04.MainProject/src/controllers/user.controller.js
@@ -226,7 +226,7 @@ const changeCurrentPassword = asyncHandler( async (req,res) =>{
 })
 
 const getCurrentUser = asyncHandler( async (req,res) =>{
-    return res.status(200).json(200,req.user,"User fetched successfully")
+    return res.status(200).json(new apiResponse(200,req.user,"User fetched successfully"))
 })
 
 const updateDetals = asyncHandler( async (req,res) => {
@@ -280,6 +280,71 @@ const updateUserCoverImage = asyncHandler( async (req,res) =>{
     },{new:true}).select("-password")
 
     return res.status(200).json(new apiResponse(200,user,"cover Image updated"))
+})
+
+const getUserChannelProfile = asyncHandler( async (req,res) =>{
+    const {username} = req.params
+    if(!(username?.trim())){
+        throw new apiError(400,"Username missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount: {
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if : {$in: [req.user?._id,"$subscribers.subscriber"]},
+                        then : true,
+                        else : false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullname:1,
+                username:1,
+                subscriberCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new apiError(401,"Channel does not exist")
+    }
+
+    return res.status(200).json(new apiResponse(200,channel[0],"User channel fetched successfully"))
 })
 
 export { 
