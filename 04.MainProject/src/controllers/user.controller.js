210c6ab6a86fccafa4760ab5fcdc6c25e3249840
unknown <ayaan.asad.2104@gmail.com>
2026-06-18 11:43:53 +0530

    Watch history controller with nested pipelines

--- 04.MainProject/src/controllers/user.controller.js
+++ 04.MainProject/src/controllers/user.controller.js
@@ -347,6 +347,52 @@ const getUserChannelProfile = asyncHandler( async (req,res) =>{
     return res.status(200).json(new apiResponse(200,channel[0],"User channel fetched successfully"))
 })
 
+const getWatchHistory = asyncHandler( async (req,res) =>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id : new mongo.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"Owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            Owner:{
+                                $first:"$Owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(new apiError(200,user[0].watchHistory,"Watch History fetched successfully"))
+})
+
 export { 
     registerUser, 
     loginUser, 
@@ -354,5 +400,6 @@ export {
     refreshAccessToken, 
     changeCurrentPassword, 
     getCurrentUser,
-    updateUserAvatar 
+    updateUserAvatar,
+    getWatchHistory 
 }
