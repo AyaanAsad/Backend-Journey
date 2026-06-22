083572dee22e1841fc56235405805965ddbef182
unknown <ayaan.asad.2104@gmail.com>
2026-06-22 15:49:39 +0530

    Like controllers done

--- 04.MainProject/src/controllers/like.controller.js
+++ 04.MainProject/src/controllers/like.controller.js
@@ -5,24 +5,78 @@ import {apiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
     //TODO: toggle like on video
+    const {videoId} = req.params
+    const likeDocument = await Like.findOne({
+        video:videoId,
+        likedBy:req.user._id
+    })
+    if(!likeDocument){
+        const like = await Like.create({
+            video:videoId,
+            likedBy:req.user._id
+        })
+        res.status(200).json(new apiResponse(200,like,"Video Liked"))
+    }
+    else{
+        await Like.deleteOne({
+            _id : likeDocument._id
+        })
+        res.status(200).json(new apiResponse(200,{},"Video Disiked"))
+    }
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
     //TODO: toggle like on comment
-
+    const {commentId} = req.params
+    const likeDocument = await Like.findOne({
+        comment:commentId,
+        likedBy:req.user._id
+    })
+    if(!likeDocument){
+        const like = await Like.create({
+            comment:commentId,
+            likedBy:req.user._id
+        })
+        res.status(200).json(new apiResponse(200,like,"Comment Liked"))
+    }
+    else{
+        await Like.deleteOne({
+            _id : likeDocument._id
+        })
+        res.status(200).json(new apiResponse(200,{},"Comment Disiked"))
+    }
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+    const {tweetId} = req.params
+    const likeDocument = await Like.findOne({
+        tweet:tweetId,
+        likedBy:req.user._id
+    })
+    if(!likeDocument){
+        const like = await Like.create({
+            tweet:tweetId,
+            likedBy:req.user._id
+        })
+        res.status(200).json(new apiResponse(200,like,"Tweet Liked"))
+    }
+    else{
+        await Like.deleteOne({
+            _id : likeDocument._id
+        })
+        res.status(200).json(new apiResponse(200,{},"Tweet Disliked"))
+    }
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const videos = await Like.find({
+        video:{$exists:true},
+        likedBy:req.user._id
+    })
+    res.status(200).json(new apiResponse(200,videos,"Videos fetched"))
 })
 
 export {
